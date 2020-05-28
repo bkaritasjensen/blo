7b5b1c8406ba98fa7cdc47e6e11040023d5727bd
Bianca Karitas jensen <bianca_karitas_jensen@hotmail.com>
2020-05-28 13:24:36 +0200

    Set up menu from contentfull

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,38 +1,36 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import get from 'lodash/get'
 import styles from './navigation.module.css'
+import Img from 'gatsby-image'
+import Layout from '../components/layout'
+import Helmet from 'react-helmet'
 
-export default () => (
-  <nav role="navigation">
-    <ul className={styles.navigation}>
-      <li className={styles.navigationDelivery}>
-        <Link to="/">
-          <img src=""/>
-          <h4></h4>
-          <p></p>
-        </Link>
-      </li>
-      <li className={styles.navigationParcels}>
-        <Link to="/">
-          <img src=""/>
-          <h4></h4>
-          <p> </p>
-        </Link>
-      </li>
-      <li className={styles.navigationQuality}>
-        <Link to="/">
-          <img src=""/>
-          <h4></h4>
-          <p></p>
-        </Link>
-      </li>
-      <li className={styles.navigationService}>
-        <Link to="/">
-          <img src=""/>
-          <h4></h4>
-          <p> </p>
-        </Link>
-      </li>
-    </ul>
-  </nav>
-)
+
+class MenuTemplate extends React.Component {
+  render() {
+    const menu = get(this.props, 'data.allContentfulMenu');
+
+    return(
+      <Layout location={this.props.location}>
+        <div style={{color: '#FFF'}}>
+          <h4>{menu.title}</h4>
+          <p>{menu.description[0].description}</p>
+        </div>
+      </Layout>
+    )
+  }
+
+}
+export default MenuTemplate
+
+
+
+export const pageQuery = graphql`
+query MenuTemplateQuery {
+  allContentfulMenu {
+    title
+  }
+}
+`
+
